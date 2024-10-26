9f7818989b5e3e317cdf2d08a75105509c68713e
glitchySid <siddheshmhatre2005@gmail.com>
2024-10-26 16:29:22 +0530

    added photo and name

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-black">
+    <div className="bg-black min-h-screen w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <MainPage />
     </div>
   );
